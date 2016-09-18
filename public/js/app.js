82b109f8034d100a26518e73d0588958b54d20d9
williamavasquez <william.a.vasquez@gmail.com>
2016-09-18 12:40:51 -0400

    working ideas

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  v1 = 80;
+  v2 = 300;
+
+      $("#vote1").html("<strong>" + v1 + "</strong><p>Votes</p>");
+      $("#vote2").html("<strong>" + v2 + "</strong><p>Votes</p>");
 
   $(".submit").on("click",function(){
       var turtle = $("#autocomplete").val().trim();
@@ -13,13 +18,18 @@ $(document).ready(function(){
     	window.location.replace(currentURL+"/form");
   })
 
-  $("tag").on("click",function(){
-  	counter ++
-  	$("whatever").html("<img src='' ><span>"+ counter + "<span>")
+  $(".voteBtn").on("click",function(){
+    v1++;
+    $("#vote1").html("<strong>" + v1 + "</strong><p>Votes</p>");
+  })
+
+  $(".voteBtn2").on("click",function(){
+    v2++;
+    $("#vote2").html("<strong>" + v2 + "</strong><p>Votes</p>");
   })
 
 })
 
-// filter on page 
+// filter on page
 //liking system
 //funding sytstem
