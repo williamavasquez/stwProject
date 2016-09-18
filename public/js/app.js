53f4e176c3b292b2bbb6645a1ba5c5308028817d
williamavasquez <william.a.vasquez@gmail.com>
2016-09-18 13:57:36 -0400

    launch final version

--- public/js/app.js
+++ public/js/app.js
@@ -40,6 +40,11 @@ $(document).ready(function(){
       setInterval(im1,6000)
       setInterval(im2,8000)
 
+      $("#input1").on("click",function(){
+        console.log("number");
+
+        $(".portfolio-container").append('<div class="portfolio-listing Ideas fashion identity other block" style="display: block;"><div class="span4 bordered"><div class="folder"><h4 class="text-center title"><a href="projects.html#proj2">Ideas</a></h4></div><div class="proj_name"><div class="clear"></div><span class="fleft author">Hugh Mongous</span><table class="icons"><tbody><tr><td class="icos_proj_sm bleft"><a class="group1" href="img/proj2.jpg" title=""><img src="img/icon_search.png" alt=""></a></td><td class="icos_proj_sm bleft"><a href="projects.html#proj2"><img src="img/icon_lock.png" alt=""></a></td></tr></tbody></table><div class="clear"></div></div><div class="folder border"><a href="projects.html#proj2"><div class="author_proj proj_2"></div></a></div><!-- <div class="folder lorem lines"><div id="project2" value="325" class="slider"></div></div> --><p class="descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate egestas sem, eu cursus ligula ullamcorper non. Curabitur tristique velit eu</p><div class="folder price topbordered"><div class="span4"><strong class="green"><span></span><span class="project_value" href="#project2"></span></strong>                                    <button class= "voteBtn" type="button"><strong>Vote</strong></button><p></p></div><div class="span4 bleft"><strong>0</strong><p>Votes</p></div><div class="span4 bleft"><strong>30 Days</strong><p>to launch</p></div></div></div></div>')
+      })
 
 
 
